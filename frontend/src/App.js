ee84290de79c2e1f774dc630a1e372dfc4881717
Gopal-Swami <Gopalswami8619@gmail.com>
2022-03-18 15:08:36 +0530

    Version 2.01.1
    Navigation Bar Designed For Desktop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import NavigationBar from './components/NavigationBar';
+import LandingPage from './screens/LandingPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <NavigationBar />
-    </>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,6 +1,51 @@
 import React from 'react';
+import logo from '../resources/HotelGRID.gif';
+import { Link } from 'react-router-dom';
+import '../styles/NavigationBarStyle.css';
+
 const NavigationBar = () => {
-  return <></>;
+  return (
+    <>
+      <div className="navigation-container">
+        <div className="navigation-logo">
+          {/* <video autoPlay muted loop width={150}>
+            <source src={logo} type="video/mp4" />
+          </video> */}
+          <img src={logo} alt="HotelGRID" id="logo" />
+        </div>
+        <div className="navigation-options">
+          <label htmlFor="nav-menu" className="navigation-menu-wrapper">
+            <i className="fa-solid fa-bars"></i>
+          </label>
+          <input
+            type="checkbox"
+            className="navigation-menu-wrapper"
+            id="nav-menu"
+          />
+          <ul className="navigation-buttons">
+            {/* <li>
+              <Link to="/">
+                <i className="fa-solid fa-user"></i> Profile
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="navigation-right-most-button">
+                Log Out
+              </Link>
+            </li> */}
+            <li>
+              <Link to="/">Log In</Link>
+            </li>
+            <li>
+              <Link to="/" className="navigation-right-most-button">
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavigationBar;
